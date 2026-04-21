370569c4598f3de3739c9a8f1278f1bc375ffc9d
Mạnh Khang <ankhangbc.2021@gmail.com>
2026-04-21 22:13:14 +0700

    login/register

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll(".input-group input");
+
+inputs.forEach(input => {
+  input.addEventListener("focus", () => {
+    input.parentElement.classList.add("active");
+  });
+
+  input.addEventListener("blur", () => {
+    if (input.value === "") {
+      input.parentElement.classList.remove("active");
+    }
+  });
+});
